fc209ae530ef3bbec5fac122c6fa5170d2c446a7
joseph shahin <josephchahine@live.com>
2022-08-14 23:41:37 +0200

    added a flash message to the loginform with the logic

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
+from crypt import methods
 from operator import imod
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
 from app.forms import LoginForm
 
@@ -20,7 +21,11 @@ def index():
     return render_template('index.html', title='Home', user=user, posts=posts)
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/index')
     return render_template('login.html', title='Log In', form=form)
