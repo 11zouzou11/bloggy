3621f66b43434c70adf704d233299aabefe4ff85
joseph shahin <josephchahine@live.com>
2022-08-15 21:24:19 +0200

    added the werkzeug password hashing

--- app/models.py
+++ app/models.py
@@ -2,8 +2,9 @@ from email.policy import default
 from enum import unique
 from re import T
 import re
-from app import db
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+from app import db
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -15,6 +16,11 @@ class User(db.Model):
     def __repr__(self):
         return '<User {}>'.format(self.username)
 
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
