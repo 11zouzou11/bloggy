5d69ba34b8f71806c11cfed1dd199f3634c743fe
joseph shahin <josephchahine@live.com>
2022-08-16 14:53:29 +0200

    fixed a bug where the user can change the username to an existing user username

--- app/forms.py
+++ app/forms.py
@@ -1,3 +1,4 @@
+from cmath import atan
 from wsgiref.validate import validator
 from xml.dom import ValidationErr
 from flask_wtf import FlaskForm
@@ -34,3 +35,15 @@ class EditProfileForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     about_me = TextAreaField('About me', validators=[Length(min=0, max=120)])
     submit = SubmitField('Submit')
+
+    def __init__(self, original_username, *args, **kwargs):
+        super(EditProfileForm, self).__init__(*args, **kwargs)
+        self.original_username = original_username
+
+    def validate_username(self, username):
+        if username.data != self.original_username:
+            user = User.query.filter_by(username=username.data).first()
+            if user is not None:
+                raise ValidationError('Please use a different username.')
+
+
