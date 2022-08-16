5af17cf3ddbaf3337ce0695a4fdb1f7cde0a2b89
joseph shahin <josephchahine@live.com>
2022-08-16 12:00:27 +0200

    added the current user template with the route to show th e profile to the current logedin user

--- app/routes.py
+++ app/routes.py
@@ -62,3 +62,20 @@ def register():
         flash('Welcome to our family!')
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
+
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {
+            'author': { user },
+            'body': 'nice day!'
+        },
+        {
+            'author': { user },
+            'body': 'nice day2222!'
+        }
+    ]
+    return render_template('user.html', user=user, posts=posts) 
