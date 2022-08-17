15cb650c156f2bfa8bf7fb3dc0ea5f0a42535953
joseph shahin <josephchahine@live.com>
2022-08-17 19:55:34 +0200

    added the bootstrap boilerplate and the requirements file

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,13 @@
+from ensurepip import bootstrap
 from pkgutil import ImpImporter
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -11,5 +15,7 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 from app import routes, models
